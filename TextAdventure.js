db08af6077df1580c0e2a5e13e44754cb05dedd5
tnarg22-dev <89484304+tnarg22-dev@users.noreply.github.com>
2021-10-28 16:00:24 -0400

    created the html page and started on some JavaScript for the text adventure

---
+++ TextAdventure.js
@@ -0,0 +1,32 @@
+// Get the text element that we will use to tell the story
+const textElement = document.getElementById("text")
+// Get the buttons we will press for out options
+const optionButtonsElement = document.getElementById("option-buttons")
+
+let state ={}
+
+function startGame(){
+    let state ={}
+    showTextNode()
+}
+
+
+//Shows current option that was picked
+function showTextNode(TextNodeIndex){
+}
+
+
+function selectOption(option){
+
+}
+const textNodes = [
+    {
+        id=1
+        Text: "this is a test"
+    }
+]
+
+
+
+
+startGame()
