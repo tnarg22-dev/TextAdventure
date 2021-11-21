5be42283b579fff5c627b5a980acbb6b1cc44723
tnarg22-dev <89484304+tnarg22-dev@users.noreply.github.com>
2021-11-21 12:50:55 -0500

    touching up the last of the java functions and giving them some reasoning for each

--- TextAdventure.js
+++ TextAdventure.js
@@ -4,43 +4,46 @@ const textElement = document.getElementById("text")
 const optionButtonsElement = document.getElementById("option-buttons")
 
 let state = {}
-
+// fuction that starts the game and displays the first text option
 function startGame() {
   state = {}
-  showTextNode(1)
+  showText(1)
 }
-
-function showTextNode(textNodeIndex) {
-  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
-  textElement.innerText = textNode.text
-  while (optionButtonsElement.firstChild) {
-    optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+// fucntion that shows the options 
+function showOption(option) {
+  return option.requiredState == null || option.requiredState(state)
+}
+// fuction that allows you to select the next option and sets your next state.
+function sltOption(option) {
+  const nextNodeId = option.nextText
+  if (nextNodeId <= 0) {
+    return startGame()
   }
+  state = Object.assign(state, option.setState)
+  showText(nextNodeId)
+}
+// this is the handler for showing options and changing states in the text adventure and incoprates the other fuctions to do so.
+// this works by finding the frist text ID then going to the next text ID based on the options the player clicked on.
+// each text ID has its own set of options that may lead to another Text ID to continue the game.
+function showText(textNodeIndex) {
+  const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+// rights the next text based on the current textnode
+  textElement.innerText = textNode.text
 
+  while (optionButtonsElement.firstChild) {
+optionButtonsElement.removeChild(optionButtonsElement.firstChild)
+  }
+// fills the buttons inner text based on each option that is avalible to choose from depending on the text
   textNode.options.forEach(option => {
     if (showOption(option)) {
       const button = document.createElement('button')
       button.innerText = option.text
       button.classList.add('btn')
-      button.addEventListener('click', () => selectOption(option))
+      button.addEventListener('click', () => sltOption(option))
       optionButtonsElement.appendChild(button)
     }
   })
 }
-
-function showOption(option) {
-  return option.requiredState == null || option.requiredState(state)
-}
-
-function selectOption(option) {
-  const nextTextNodeId = option.nextText
-  if (nextTextNodeId <= 0) {
-    return startGame()
-  }
-  state = Object.assign(state, option.setState)
-  showTextNode(nextTextNodeId)
-}
-
 const textNodes = [
     {
         id: 1,
